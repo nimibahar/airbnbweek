3bb1e42bab3e3d2aa195f0d31b126e748a6233e1
Ricardo Furuta <ricardofuruta@yahoo.com.br>
2016-03-29 14:09:59 +0200

    created the profile module and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328161648) do
+ActiveRecord::Schema.define(version: 20160329113838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,18 @@ ActiveRecord::Schema.define(version: 20160328161648) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "fname"
+    t.string   "lname"
+    t.text     "description"
+    t.string   "city"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -57,4 +69,5 @@ ActiveRecord::Schema.define(version: 20160328161648) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "profiles", "users"
 end
