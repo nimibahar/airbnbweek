362f35f63a7239f8752c0c753bac940890fc7ae8
Nimrod Bahar <nimrodbahar@gmail.com>
2016-03-30 16:02:24 +0200

    a before action method - authenticate user added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,8 @@
 class BookingsController < ApplicationController
    before_action :set_flat, only: [:new, :create, :show, :edit, :update, :destroy]
 
+   before_action :authenticate_user!
+
   # GET /bookings
   # GET /bookings.json
   def index
